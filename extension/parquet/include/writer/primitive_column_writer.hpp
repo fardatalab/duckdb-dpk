6610e58c50cdde9290e6e9dbcf44f97e4d67fad0
Jason Hu <hujason@hotmail.com>
2026-02-17 15:49:33 -0500

    use 4MB page size

--- extension/parquet/include/writer/primitive_column_writer.hpp
+++ extension/parquet/include/writer/primitive_column_writer.hpp
@@ -63,10 +63,12 @@ public:
 
 	//! We limit the uncompressed page size to 100MB
 	//! The max size in Parquet is 2GB, but we choose a more conservative limit
-	static constexpr const idx_t MAX_UNCOMPRESSED_PAGE_SIZE = 104857600ULL;
+	// jason: change it to 4MB
+	static constexpr const idx_t MAX_UNCOMPRESSED_PAGE_SIZE = 4194304ULL; // 104857600ULL;
 	//! Dictionary pages must be below 2GB. Unlike data pages, there's only one dictionary page.
 	//! For this reason we go with a much higher, but still a conservative upper bound of 1GB;
-	static constexpr const idx_t MAX_UNCOMPRESSED_DICT_PAGE_SIZE = 1073741824ULL;
+	// jason: change it to 64MB
+	static constexpr const idx_t MAX_UNCOMPRESSED_DICT_PAGE_SIZE = 67108864ULL; // 1073741824ULL;
 
 public:
 	unique_ptr<ColumnWriterState> InitializeWriteState(duckdb_parquet::RowGroup &row_group) override;
